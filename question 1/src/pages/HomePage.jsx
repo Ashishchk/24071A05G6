a106228846683615338e8c6b067b74278a1544dd
Korra Ashish Chauhan <24071a05g6@vnrvjiet.in>
2026-04-27 12:11:44 +0530

    Update HomePage.jsx

--- question 1/src/pages/HomePage.jsx
+++ question 1/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ function HomePage() {
   return (
     <section className="page">
       <h1>Learn Gym Management</h1>
-      <p>This simple student-style system shows registration, login, membership plans, payment, and contact pages. Use the links above to move between pages.</p>
+      <p>This system shows registration, login, membership plans, payment, and contact pages. Use the links above to move between pages.</p>
     </section>
   )
 }
